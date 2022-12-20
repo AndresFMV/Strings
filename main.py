1e575acfb9fc64e14f230193f75c42e9f9267597
AndresFMV <120723097+AndresFMV@users.noreply.github.com>
2022-12-20 11:39:55 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ goal_1 = 54
 scorers =  f'{scorer_a} {str(goal_0)}, {scorer_b} {str(goal_1)}'
 report = f'{scorer_a} scored in the {goal_0}nd minute\n{scorer_b} scored in the {goal_1}th minute'
 player = 'Ruud Gullit'
-first_name_slice = slice(0,4)
-first_name = player[first_name_slice]
+first_name_find = player[player.find('')]
+first_name_slice = slice(1,4)
+first_name = first_name_find + player[first_name_slice]
+last_name_find = player[player.find('')+5]
+last_name_slice = slice(6,11)
 last_name_len = len(player[5:])
+last_name = last_name_find + player[last_name_slice]
 name_short = f'{player[0]}. {player[5:]}'
 chant = f'{first_name}! {first_name}! {first_name}! {first_name}!'
 good_chant = chant != chant[-1]
-print(name_short)
+print(last_name)
